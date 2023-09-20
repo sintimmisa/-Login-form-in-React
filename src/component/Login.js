69cc13d5a9f0906687ccdb5f92797e8e5d70177b
Godfred D. Sintim-Misah <sintim.misa@gmail.com>
2023-09-20 02:46:36 +0000

    styled form

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import './myStyle.css'
 
 const Login = () => {
 
@@ -10,7 +12,7 @@ const Login = () => {
 
   return (
     <div className='container'>
-        <div className='form_container'>
+        <div className='form-container'>
             <h2>Login Form</h2>
             <form>
                 <div className='form-group'>
@@ -30,7 +32,7 @@ const Login = () => {
                     <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
 
                 </div>
-                <button>Login</button>
+                <button className='btn'>Login</button>
             </form>
         </div>
     </div>
